ad9df8382fe9161c9dd7414c662b6a002d7b06a9
潘柯宇 <pankeyu@bjca.org.cn>
2023-02-23 18:31:08 +0800

    11

---
+++ src/js/api/commonUtil.ts
@@ -0,0 +1,66 @@
+export const typeColor = [
+  {
+    "billTypeColor": "#4f84db",
+    "billTypeName": "饮食",
+  },
+  {
+    "billTypeColor": "#12c112",
+    "billTypeName": "房租",
+  },
+  {
+    "billTypeColor": "#9dc3ff",
+    "billTypeName": "交通",
+  },
+  {
+    "billTypeColor": "#ffb4ff",
+    "billTypeName": "娱乐",
+  },
+  {
+    "billTypeColor": "#00ffc4",
+    "billTypeName": "游戏",
+  },
+  {
+    "billTypeColor": "#ffff3c",
+    "billTypeName": "生活用品",
+  },
+  {
+    "billTypeColor": "#f45252",
+    "billTypeName": "超市",
+  },
+  {
+    "billTypeColor": "#e6c662",
+    "billTypeName": "日常消费",
+  },
+  {
+    "billTypeColor": "#45C2E0",
+    "billTypeName": "衣物",
+  },
+  {
+    "billTypeColor": "#A3D400",
+    "billTypeName": "网费-话费",
+  },
+  {
+    "billTypeColor": "#C1EBDD",
+    "billTypeName": "水电气",
+  },
+  {
+    "billTypeColor": "#FFC851",
+    "billTypeName": "药品",
+  },
+  {
+    "billTypeColor": "#1869A0",
+    "billTypeName": "花呗",
+  },
+  {
+    "billTypeColor": "#f2b7b7",
+    "billTypeName": "电子设备",
+  },
+  {
+    "billTypeColor": "#649b4b",
+    "billTypeName": "红包",
+  },
+  {
+    "billTypeColor": "#9F79EE",
+    "billTypeName": "会员",
+  }
+]
